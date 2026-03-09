6817eb184c2ec99ff6a14e9b273a8efddff1e9fd
dani2188 <oth.lahmer@gmail.com>
2026-03-09 14:52:29 +0100

    first version

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template, send_file
+import subprocess
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+
+        file = request.files["pdf"]
+
+        input_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        output_path = os.path.join(UPLOAD_FOLDER, "compressed_" + file.filename)
+
+        file.save(input_path)
+
+        command = [
+            "gswin64c",
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dPDFSETTINGS=/screen",
+            "-dNOPAUSE",
+            "-dQUIET",
+            "-dBATCH",
+            f"-sOutputFile={output_path}",
+            input_path
+        ]
+
+        subprocess.run(command)
+
+        return send_file(output_path, as_attachment=True)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
